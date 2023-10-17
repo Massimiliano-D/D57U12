cc6ee84859cca28574244780cc0d24dd4af03157
Massimiliano-D <137884771+Massimiliano-D@users.noreply.github.com>
2023-10-17 16:58:25 +0200

    last commit

--- Eventi/src/main/java/org/example/Main.java
+++ Eventi/src/main/java/org/example/Main.java
@@ -1,8 +1,14 @@
 package org.example;
 
+import entities.Evento;
+import entities.TipoEvento;
+
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Evento even1 = new Evento("Peppino di Capri", LocalDate.of(2022, 10, 26), "Nostalgia BOOMER", TipoEvento.PUBBLICO, 200000);
+        Evento even2 = new Evento("Matrimonio", LocalDate.of(2021, 8, 15), "Roviniamo Estati", TipoEvento.PRIVATO, 200);
     }
 }
